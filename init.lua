113dd59c17b82a207c92efe59ae11dffdf7001ea
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-02-07 11:07:06 +0100

    Use OSC 52 to forward clipboard over SSH

--- init.lua
+++ init.lua
@@ -152,6 +152,21 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 
+-- Forward clipboard
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
+end
+
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
